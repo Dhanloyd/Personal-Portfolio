c4524c4ea8890716773a099a1801b8e2d3e883b3
Dhanloyd <93707472+Dhanloyd@users.noreply.github.com>
2025-06-09 11:04:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Accessibility: Add user-is-tabbing class when user is navigating via keyboard (tab)
+(function () {
+  function handleFirstTab(e) {
+    if (e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing');
+      window.removeEventListener('keydown', handleFirstTab);
+      window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+  }
+  function handleMouseDownOnce() {
+    document.body.classList.remove('user-is-tabbing');
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+  }
+  window.addEventListener('keydown', handleFirstTab);
+})();
